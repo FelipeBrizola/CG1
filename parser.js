bb859dd29b2e575c114ce410f39f5d697dbd8c81
felipebrizola <felipe_brizola@hotmail.com>
2016-09-04 20:20:26 -0300

    parser.js

--- parser.js
+++ parser.js
@@ -1,1 +1,32 @@
 /* Transforma .txt em arquivo json estruturado  */
+
+const fs  = require("fs");
+
+fs.readFile('BR-01/Paths_D.txt', function(err, f) {
+    var peoplesTxt = f.toString().split('\n'),
+        i = 1,
+        pointsArray = [],
+        peoples = [],
+        people;
+
+    for (i; i < peoplesTxt.length; i += 1) {
+        pointsArray = peoplesTxt[i].replace(/^[^(]*\(/, "").replace(/\)[^(]*$/, "").split(/\)[^(]*\(/); 
+        
+        people = {
+            'id': peoplesTxt[i].split('\t')[0],
+            'points': []
+        }
+
+        pointsArray.forEach(function (point) {
+            people.points.push({
+                'x': point.split(',')[0],
+                'y': point.split(',')[1],
+                't': point.split(',')[2]
+            });
+        });
+
+        if (people.id && people.points.length > 0)
+            peoples.push(people);
+    }
+    return peoples;
+});
